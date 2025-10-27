1409120b3ae758e7c3e0412ba90db475536fe51f
Ariel Wijaya <arielkusuma17@gmail.com>
2025-10-27 22:03:17 +0700

    refactor: resize img

--- components/sections/HeroSection.tsx
+++ components/sections/HeroSection.tsx
@@ -8,23 +8,23 @@ interface HeroSectionProps {
 
 export default function HeroSection({ searchQuery, onSearchChange }: HeroSectionProps) {
   const handleScrollDown = () => {
-  const categorySection = document.getElementById('category-section');
-  if (categorySection) {
-    const navbarHeight = 50;
-    const elementPosition = categorySection.getBoundingClientRect().top;
-    const offsetPosition = elementPosition + window.pageYOffset - navbarHeight;
+    const categorySection = document.getElementById("category-section");
+    if (categorySection) {
+      const navbarHeight = 50;
+      const elementPosition = categorySection.getBoundingClientRect().top;
+      const offsetPosition = elementPosition + window.pageYOffset - navbarHeight;
 
-    window.scrollTo({
-      top: offsetPosition,
-      behavior: 'smooth'
-    });
-  }
-};
+      window.scrollTo({
+        top: offsetPosition,
+        behavior: "smooth",
+      });
+    }
+  };
 
   return (
     <div
       className="relative min-h-screen bg-cover bg-center bg-no-repeat"
-      style={{ backgroundImage: "url('/images/hero-bg.png')" }}
+      style={{ backgroundImage: "url('/images/hero-bg.webp')" }}
     >
       <div className="container mx-auto flex flex-col justify-center gap-5 min-h-[100vh] px-12">
         <h1 className="text-background text-5xl sm:text-6xl font-bold">
@@ -45,7 +45,7 @@ export default function HeroSection({ searchQuery, onSearchChange }: HeroSection
 
       <div className="absolute bottom-8 right-8 md:right-12 flex items-center gap-3 text-white">
         <p className="text-sm font-medium hidden md:block">Scroll Down</p>
-        <button 
+        <button
           onClick={handleScrollDown}
           className="p-3 border-2 border-white rounded-full hover:bg-white/10 hover:border-primary-content hover:text-primary-content transition-all group"
         >
