3ec9047c206bf1223bf24fc602d44fdda5022c7c
hikmalmn <hikmal.233040091@mail.unpas.ac.id>
2025-10-31 21:49:10 +0700

    fixed: wrapped navbar with suspense

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,15 @@
 import Navbar from "@/components/layout/Navbar";
 import Footer from "@/components/layout/Footer";
+import {Suspense} from "react";
 
 export default function Layout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <div>
-      <Navbar />
+      <Suspense fallback={<div />}>
+        <Navbar />
+      </Suspense>
 
       {children}
 
