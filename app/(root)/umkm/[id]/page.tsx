9dcee94cf08685008d35c00d4a125679d926dff9
Ariel Wijaya <arielkusuma17@gmail.com>
2025-11-01 20:31:37 +0700

    fix: error data

--- app/(root)/umkm/[id]/page.tsx
+++ app/(root)/umkm/[id]/page.tsx
@@ -1,20 +1,21 @@
-import { umkmData } from "@/data/umkm";
+"use client";
 import { notFound } from "next/navigation";
 import React from "react";
 import ProductCard from "@/components/ui/detail/ProductCard";
 import LocationMap from "@/components/ui/map/LocationMap";
 import Breadcrumbs from "@/components/ui/Breadcrumbs";
 import UmkmProfile from "@/components/ui/detail/UmkmProfile";
+import { useUmkm } from "@/hooks/useUmkm";
 
 export default function page({ params }: { params: Promise<{ id: string }> }) {
   const { id } = React.use(params);
+  const umkmData = useUmkm();
   const umkm = umkmData.find((item) => item.id === parseInt(id));
 
   if (!umkm) notFound();
 
   return (
     <div className="flex flex-col gap-20 pb-8">
-
       {/* Hero */}
       <section
         style={{
@@ -22,18 +23,13 @@ export default function page({ params }: { params: Promise<{ id: string }> }) {
           backgroundSize: "cover",
           backgroundPosition: "center",
         }}
-        className="relative w-full h-auto bg-center bg-cover overflow-hidden">
-
+        className="relative w-full h-auto bg-center bg-cover overflow-hidden"
+      >
         <div className="absolute inset-0 bg-white/60 backdrop-blur-xs" />
 
         <div className="relative container mx-auto px-6 lg:px-12 py-16">
           <div className="mb-5">
-            <Breadcrumbs
-              items={[
-                { label: "Home", href: "/" },
-                { label: umkm.title },
-              ]}
-            />
+            <Breadcrumbs items={[{ label: "Home", href: "/" }, { label: umkm.name }]} />
           </div>
 
           {/* UMKM Profile */}
@@ -43,19 +39,18 @@ export default function page({ params }: { params: Promise<{ id: string }> }) {
 
       {/* List Card Product */}
       <section className="container mx-auto px-6 lg:px-12">
-        <h1 className="text-center text-2xl font-bold mb-5">Produk <span className="text-primary-content">Kami</span>
+        <h1 className="text-center text-2xl font-bold mb-5">
+          Produk <span className="text-primary-content">Kami</span>
         </h1>
 
         <div className="flex gap-15 overflow-x-auto w-full h-auto py-2 scrollbar-hide snap-x snap-mandatory">
-          {
-            Array(6)
-              .fill(0)
-              .map((_, i) => (
-                <div key={i} className="snap-center flex-shrink-0 w-[250px]">
-                  <ProductCard/>
-                </div>
-              ))
-          }
+          {Array(6)
+            .fill(0)
+            .map((_, i) => (
+              <div key={i} className="snap-center flex-shrink-0 w-[250px]">
+                <ProductCard />
+              </div>
+            ))}
         </div>
       </section>
 
@@ -67,7 +62,8 @@ export default function page({ params }: { params: Promise<{ id: string }> }) {
           </h1>
 
           <p className="text-center md:text-left max-w-150">
-            Yuk, mampir langsung ke lokasi kami dan dukung usaha lokal di sekitarmu. Setiap kunjunganmu berarti besar bagi kami.
+            Yuk, mampir langsung ke lokasi kami dan dukung usaha lokal di sekitarmu. Setiap kunjunganmu berarti besar
+            bagi kami.
           </p>
         </div>
         <LocationMap umkm={umkm} mode={"detail"} />
--- components/ui/detail/UmkmProfile.tsx
+++ components/ui/detail/UmkmProfile.tsx
@@ -1,17 +1,17 @@
 "use client";
 
-import React, {useEffect, useRef, useState} from 'react';
+import React, { useEffect, useRef, useState } from "react";
 import ImageGallery from "@/components/ui/detail/ImageGallery";
 import UmkmInfo from "@/components/ui/detail/UmkmInfo";
-import {UmkmItem} from "@/types/umkm";
-import {gsap} from "gsap";
+import { UmkmItem } from "@/types/umkm";
+import { gsap } from "gsap";
 import UmkmDescription from "@/components/ui/detail/UmkmDescription";
 
 type UmkmProfileProps = {
   umkm: UmkmItem;
 };
 
-const UmkmProfile: React.FC<UmkmProfileProps> = ({umkm}) => {
+const UmkmProfile: React.FC<UmkmProfileProps> = ({ umkm }) => {
   const [isShowDescription, setIsShowDescription] = useState(false);
   const modalRef = useRef<HTMLDivElement>(null);
   const descriptionRef = useRef<HTMLDivElement>(null);
@@ -77,68 +77,50 @@ const UmkmProfile: React.FC<UmkmProfileProps> = ({umkm}) => {
     if (isShowDescription && descriptionRef.current) {
       gsap.fromTo(
         descriptionRef.current,
-        {opacity: 0, x: -200},
-        {opacity: 1, x: 0, duration: 0.6, ease: "power2.out"}
+        { opacity: 0, x: -200 },
+        { opacity: 1, x: 0, duration: 0.6, ease: "power2.out" }
       );
     } else if (!isMobile && !isShowDescription && modalRef.current) {
-      gsap.fromTo(
-        modalRef.current,
-        {opacity: 0, x: 200},
-        {opacity: 1, x: 0, duration: 0.6, ease: "power2.out"}
-      );
+      gsap.fromTo(modalRef.current, { opacity: 0, x: 200 }, { opacity: 1, x: 0, duration: 0.6, ease: "power2.out" });
     }
-
   }, [isShowDescription, isMounted]);
 
   return (
     <>
       <div
         ref={showDesktopDesc ? descriptionRef : modalRef}
-        className={`relative grid grid-cols-1 lg:grid-cols-2 ${showDesktopDesc ? 'gap-8' : 'gap-15'}`}
+        className={`relative grid grid-cols-1 lg:grid-cols-2 ${showDesktopDesc ? "gap-8" : "gap-15"}`}
       >
         {/* UMKM Gallery */}
         <div className={showDesktopDesc ? "opacity-0" : ""}>
-          <ImageGallery images={umkm.images} title={umkm.title}/>
+          <ImageGallery images={umkm.imageUrl} title={umkm.name} />
         </div>
-        {
-          showDesktopDesc ? (
-            /* UMKM Description */
-            <UmkmDescription
-              title={umkm.title}
-              description={umkm.description}
-              handleBackToProfile={handleBackToProfile}
-            />
-          ) : (
-            /* UMKM Information */
-            <UmkmInfo
-              category={umkm.category}
-              title={umkm.title}
-              address={umkm.address}
-              handleShowDescription={handleShowDescription}
-            />
-          )
-        }
+        {showDesktopDesc ? (
+          /* UMKM Description */
+          <UmkmDescription title={umkm.name} description={umkm.description} handleBackToProfile={handleBackToProfile} />
+        ) : (
+          /* UMKM Information */
+          <UmkmInfo
+            category={umkm.subCategory}
+            title={umkm.name}
+            address={umkm.address}
+            handleShowDescription={handleShowDescription}
+          />
+        )}
       </div>
 
       {/* Description on Mobile */}
-      {
-        isShowDescription && isMobile && (
-          <div
-            ref={descriptionRef}
-            className="fixed inset-0 flex items-center justify-center w-full h-[100vh] bg-white z-30"
-          >
-            {/* UMKM Description */}
-            <UmkmDescription
-              title={umkm.title}
-              description={umkm.description}
-              handleBackToProfile={handleBackToProfile}
-            />
-          </div>
-        )
-      }
+      {isShowDescription && isMobile && (
+        <div
+          ref={descriptionRef}
+          className="fixed inset-0 flex items-center justify-center w-full h-[100vh] bg-white z-30"
+        >
+          {/* UMKM Description */}
+          <UmkmDescription title={umkm.name} description={umkm.description} handleBackToProfile={handleBackToProfile} />
+        </div>
+      )}
     </>
-  )
-    ;
+  );
 };
 
 export default UmkmProfile;
